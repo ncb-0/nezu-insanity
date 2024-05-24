df5aad50fd9cfe3fe0e248711d3ecd4b0114f506
nezu <mel@nezu.world>
2024-05-23 23:21:09 -0400

    fuck uuuu!

--- app/src/lib/sanity/api.ts
+++ app/src/lib/sanity/api.ts
@@ -1,21 +1,7 @@
-import {
-	PUBLIC_SANITY_DATASET,
-	PUBLIC_SANITY_PROJECT_ID,
-	PUBLIC_SANITY_API_VERSION,
-	PUBLIC_SANITY_STUDIO_URL,
-} from "$env/static/public";
+export const dataset = "production";
 
-export function assertEnvVar<T>(value: T | undefined, name: string): T {
-	if (value === undefined) {
-		throw new Error(`Missing environment variable: ${name}`);
-	}
-	return value;
-}
+export const projectId = "npvamhe5";
 
-export const dataset = PUBLIC_SANITY_DATASET;
+export const apiVersion = "2024-05-23";
 
-export const projectId = PUBLIC_SANITY_PROJECT_ID;
-
-export const apiVersion = PUBLIC_SANITY_API_VERSION || "2024-03-15";
-
-export const studioUrl = PUBLIC_SANITY_STUDIO_URL || "http://localhost:3333";
+export const studioUrl = "http://localhost:3333";
