d0d4bb514b321f8ae5d62f87e9df4c064b2c2128
nezu <mel@nezu.world>
2024-05-23 23:19:00 -0400

    lalala fuck you ts!

--- app/src/lib/sanity/api.ts
+++ app/src/lib/sanity/api.ts
@@ -5,15 +5,16 @@ import {
 	PUBLIC_SANITY_STUDIO_URL,
 } from "$env/static/public";
 
-export const dataset = assertEnvVar(
-	PUBLIC_SANITY_DATASET,
-	"PUBLIC_SANITY_DATASET"
-);
+export function assertEnvVar<T>(value: T | undefined, name: string): T {
+	if (value === undefined) {
+		throw new Error(`Missing environment variable: ${name}`);
+	}
+	return value;
+}
 
-export const projectId = assertEnvVar(
-	PUBLIC_SANITY_PROJECT_ID,
-	"PUBLIC_SANITY_PROJECT_ID"
-);
+export const dataset = PUBLIC_SANITY_DATASET;
+
+export const projectId = PUBLIC_SANITY_PROJECT_ID;
 
 export const apiVersion = PUBLIC_SANITY_API_VERSION || "2024-03-15";
 
