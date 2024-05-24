238bde4e24bc00f610cf78acc31d49c4474db9df
nezu <mel@nezu.world>
2024-05-23 23:27:04 -0400

    i hate ts!

--- app/src/lib/server/sanity/api.ts
+++ app/src/lib/server/sanity/api.ts
@@ -1,4 +1,3 @@
 import { SANITY_API_READ_TOKEN } from "$env/static/private";
-import { assertEnvVar } from "$lib/sanity/api";
 
 export const token = SANITY_API_READ_TOKEN;
--- app/src/types.d.ts
+++ app/src/types.d.ts
@@ -3,4 +3,5 @@ declare module "$env/static/public" {
 	export const PUBLIC_SANITY_PROJECT_ID: string;
 	export const PUBLIC_SANITY_API_VERSION: string;
 	export const PUBLIC_SANITY_STUDIO_URL: string;
+	export const SANITY_API_READ_TOKEN: string;
 }
