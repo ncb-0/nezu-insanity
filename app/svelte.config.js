4618cd7ba83268e3bf2fda723932266ad35e0ac4
nezu <mel@nezu.world>
2024-05-23 23:11:11 -0400

    env fix t1

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		env: { dir: "./" },
 		adapter: adapter(),
 		paths: {
 			relative: false,
