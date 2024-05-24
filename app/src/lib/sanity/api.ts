755ab933e2c410e4d7dae873c3836a6049539308
nezu <mel@nezu.world>
2024-05-23 23:17:52 -0400

    try again…

--- app/src/lib/sanity/api.ts
+++ app/src/lib/sanity/api.ts
@@ -5,13 +5,6 @@ import {
 	PUBLIC_SANITY_STUDIO_URL,
 } from "$env/static/public";
 
-export function assertEnvVar<T>(value: T | undefined, name: string): T {
-	if (value === undefined) {
-		throw new Error(`Missing environment variable: ${name}`);
-	}
-	return value;
-}
-
 export const dataset = assertEnvVar(
 	PUBLIC_SANITY_DATASET,
 	"PUBLIC_SANITY_DATASET"
