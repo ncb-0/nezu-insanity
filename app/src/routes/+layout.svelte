b0389e2b32b628f25bfb8dd90a98cb2830838f2c
nezu <mel@nezu.world>
2024-05-24 20:21:56 -0400

    disable visual editing LOLLL

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,26 +3,8 @@ import "$lib/styles/style.css";
 import "$lib/styles/fonts.css";
 import { page } from "$app/stores";
 import Navbar from "$lib/components/Navbar.svelte";
-import { enableVisualEditing } from "@sanity/visual-editing";
-import { useLiveMode } from "@sanity/svelte-loader";
-import { client } from "$lib/sanity/client";
-let PUBLIC_SANITY_STUDIO_URL = "localhost:3333";
-import { onMount } from "svelte";
-console.log(PUBLIC_SANITY_STUDIO_URL);
+
 export let data;
-
-onMount(() => enableVisualEditing());
-
-onMount(() =>
-	useLiveMode({
-		// If `stega.studioUrl` was not provided to the client instance in `sanity.ts`, a studioUrl should be provided here
-		studioUrl: PUBLIC_SANITY_STUDIO_URL,
-		// ...or alternatively provide the stega client directly
-		// client: client.withConfig({
-		//   stega: true
-		// })
-	})
-);
 </script>
 
 <svelte:head>
