04206ca45cc16f470606c3e65cbd99042a891ae3
nezu <mel@nezu.world>
2024-05-25 21:02:51 -0400

    oops!

--- app/src/routes/(pages)/[...slug]/+page.svelte
+++ app/src/routes/(pages)/[...slug]/+page.svelte
@@ -18,13 +18,6 @@ $: ({ data: post } = $q);
 
 <svelte:head>
 	<title>{post.shortTitle} | nezu.world</title>
-	<meta property="og:title" content="The Rock" />
-	<meta property="og:type" content="video.movie" />
-	<meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-	<meta
-		property="og:image"
-		content="https://ia.media-imdb.com/images/rock.jpg"
-	/>
 </svelte:head>
 
 <article>
