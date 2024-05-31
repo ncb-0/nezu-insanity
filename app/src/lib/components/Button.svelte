dd65ee5cf430d9aec389f396d59994322df3c887
nezu <mel@nezu.world>
2024-05-30 21:51:46 -0400

    upload pdf

--- app/src/lib/components/Button.svelte
+++ app/src/lib/components/Button.svelte
@@ -5,9 +5,9 @@ export let { buttonText, href, inline } = portableText.value;
 </script>
 
 {#if inline}
-	<a {href} class="button">{buttonText}</a>
+	<a {href} class="button" target="_blank">{buttonText}</a>
 {:else}
 	<h3>
-		<a {href} class="button">{buttonText}</a>
+		<a {href} class="button" target="_blank"> {buttonText}</a>
 	</h3>
 {/if}
