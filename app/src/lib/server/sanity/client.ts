5e42fc48365ccd9f8c98dd74410bda7342ee9577
nezu <mel@nezu.world>
2024-06-07 17:23:45 -0400

    env 2

--- app/src/lib/server/sanity/client.ts
+++ app/src/lib/server/sanity/client.ts
@@ -1,6 +1,6 @@
 import { client } from "$lib/sanity/client";
 import { token } from "$lib/server/sanity/api";
-import { SANITY_API_READ_TOKEN } from "$env/static/public";
+import { SANITY_API_READ_TOKEN } from "$env/static/private";
 
 export const serverClient = client.withConfig({
 	token: SANITY_API_READ_TOKEN,
--- app/src/types.d.ts
+++ app/src/types.d.ts
@@ -3,5 +3,8 @@ declare module "$env/static/public" {
 	export const PUBLIC_SANITY_PROJECT_ID: string;
 	export const PUBLIC_SANITY_API_VERSION: string;
 	export const PUBLIC_SANITY_STUDIO_URL: string;
+}
+
+declare module "$env/static/private" {
 	export const SANITY_API_READ_TOKEN: string;
 }
