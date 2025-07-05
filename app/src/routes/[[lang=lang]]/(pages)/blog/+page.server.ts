aa92decd8a363ed63a0c831f35b043c1aa475939
nezu <mel@nezu.world>
2025-07-05 14:50:16 -0400

    derrrploy

--- app/src/routes/[[lang=lang]]/(pages)/blog/+page.server.ts
+++ app/src/routes/[[lang=lang]]/(pages)/blog/+page.server.ts
@@ -3,7 +3,10 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
 	const { loadQuery } = event.locals;
-	const initial = await loadQuery<BlogPost[]>(query);
+
+	const initial = await loadQuery<BlogPost[]>(query, {
+		lang: event.params.lang || "en",
+	});
 
 	// We pass the data in a format that is easy for `useQuery` to consume in the
 	// corresponding `+page.svelte` file, but you can return the data in any
